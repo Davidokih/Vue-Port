21b644af970c43af6aa63f1f9cf2bb2f9341f205
Davidokih <davidyole023@gmail.com>
2024-04-17 11:33:58 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: '#0f182c',
+        background: '#0a192f',
         header: '#131b2e',
         button: '#64ffda',
         primary: '#8892b0',
         secondary: '#0a192f',
         cardColor: '#112240',
-        cardSkill: '#838dab',
+        cardSkill: '#a8b2d1',
       },
       fontSize: {
         clamp: "clamp(40px, 8vw, 70px)",

4f73743ca721cb8c94fa9d4efe04206cc41b493a
Davidokih <davidyole023@gmail.com>
2024-04-24 12:51:50 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import About from './Pages/About.vue';
 import Hero from './Pages/Hero.vue';
 import Experience from './Pages/Experience.vue';
 import Projects from './Pages/Projects.vue';
+import Contact from './Pages/Contact.vue';
 import './assets/App.css';
 import { ref } from 'vue';
 
@@ -27,17 +28,16 @@ const objectProps = {
 
 <template>
   <div class="bg-secondary text-primary flex items-center justify-center font-family">
-    <div class="w-[90%]">
+    <div class="w-[90%] max-w-[1200px]">
       <Header />
       <Hero />
       <About />
       <Experience />
       <Projects />
+      <Contact />
       <!-- <Counter v-bind=" objectProps " />
       <InputsTypes />
       <FetchData /> -->
     </div>
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/Pages/Contact.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Button from '@/components/Button.vue';
+import { ref } from 'vue';
+const title = ref('Message Me');
+const padding = ref('py-4 px-5');
+</script>
+
+<template>
+    <div class="flex items-center justify-center">
+        <div>
+            <div class='font-mono text-[22px] text-button'>04. Contact Me</div>
+        </div>
+        <Button :title=" title " :padding=" padding " />
+    </div>
+</template>

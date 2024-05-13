0acf45b6ca007594ea17e284a6d1765b96636538
Davidokih <davidyole023@gmail.com>
2024-05-13 06:45:51 +0100

    update

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="flex items-center justify-center fixed bottom-0 h-14 bg-[#112240] w-full z-40">
         <div class="flex justify-between items-center w-[90%]">
-            <div class=" hover:text-button cursor-pointer">Icon1</div>
+            <div class=" hover:text-button cursor-pointer"><font-awesome-icon icon="fa-solid fa-user-tie"
+                    style="color: #63E6BE;" /></div>
             <div class=" hover:text-button cursor-pointer">Icon2</div>
             <div class=" hover:text-button cursor-pointer">Icon3</div>
             <div class=" hover:text-button cursor-pointer">Icon4</div>
@@ -10,4 +11,9 @@
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUserTie);
+</script>

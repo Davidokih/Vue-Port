7a5d293ae0a5962e7409626d2d33e2d2843da4c3
Davidokih <davidyole023@gmail.com>
2024-05-13 07:10:34 +0100

    update

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex items-center justify-center fixed bottom-0 h-14 bg-[#112240] w-full z-40">
+    <div
+        class="hidden tablet:flex items-center justify-center fixed bottom-0 h-14 bg-[#112240] w-full z-40 text-navClamp">
         <div class="flex justify-between items-center w-[90%]">
-            <div class=" hover:text-button cursor-pointer"><font-awesome-icon icon="fa-solid fa-user-tie"
-                    style="color: #63E6BE;" /></div>
-            <div class=" hover:text-button cursor-pointer">Icon2</div>
-            <div class=" hover:text-button cursor-pointer">Icon3</div>
+            <div class=" hover:text-button cursor-pointer"><font-awesome-icon icon="fa-solid fa-user-tie" /></div>
+            <div class=" hover:text-button cursor-pointer"><font-awesome-icon icon="fa-solid fa-user-tie" /></div>
+            <div class=" hover:text-button cursor-pointer"><font-awesome-icon icon="fa-solid fa-laptop-code" /></div>
             <div class=" hover:text-button cursor-pointer">Icon4</div>
             <div class=" hover:text-button cursor-pointer">Icon5</div>
         </div>
@@ -13,7 +13,7 @@
 
 <script setup>
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserTie } from '@fortawesome/free-solid-svg-icons';
+import { faUserTie, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faUserTie);
+library.add(faUserTie, faLaptopCode);
 </script>
